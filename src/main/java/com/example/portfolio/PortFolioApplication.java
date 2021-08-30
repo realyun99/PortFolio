86c783aec290b15c390232a16f72ca1d5184cb20
rhdtls1206@naver.com <rhdtls1206@naver.com>
2021-08-30 16:35:00 +0900

    Initial commit

---
+++ src/main/java/com/example/portfolio/PortFolioApplication.java
@@ -0,0 +1,13 @@
+package com.example.portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortFolioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortFolioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/portfolio/PortFolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortFolioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
